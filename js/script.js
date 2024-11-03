41b3c0ceb0aa22db92f79b52e4ed4c9dac8acf9a
Anas Ismail Khan <anasismail28@gmail.com>
2024-11-03 17:18:33 +0500

    hero, services

---
+++ js/script.js
@@ -0,0 +1,18 @@
+var typed = new Typed("#rotating-head", {
+    strings: ["Technology", "Branding", "Marketing"],
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 1500,
+    startDelay: 500,
+    loop: true,
+});
+var typed2 = new Typed("#rotating-desc", {
+    strings: [
+        "We're a turn-key agency with a multi-disciplinary approach to every project.\
+            From development and\
+            branding to SEO, SEM, and SMM, we handle every detail to grow your business from concept to\
+            completion.<br><br> Whether you need a digital presence, strategic growth, or a full-scale launch, Pépin\
+            Raisin delivers results that make an impact.",
+    ],
+    typeSpeed: 20,
+});
