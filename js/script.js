60b01b04c037349100a83f6b115b009934cb2c01
Anas Ismail Khan <anasismail28@gmail.com>
2024-11-04 01:06:16 +0500

    hal jordan

--- js/script.js
+++ js/script.js
@@ -6,13 +6,3 @@ var typed = new Typed("#rotating-head", {
     startDelay: 500,
     loop: true,
 });
-var typed2 = new Typed("#rotating-desc", {
-    strings: [
-        "We're a turn-key agency with a multi-disciplinary approach to every project.\
-            From development and\
-            branding to SEO, SEM, and SMM, we handle every detail to grow your business from concept to\
-            completion.<br><br> Whether you need a digital presence, strategic growth, or a full-scale launch, Pépin\
-            Raisin delivers results that make an impact.",
-    ],
-    typeSpeed: 20,
-});
