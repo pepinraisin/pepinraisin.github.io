de9a4a5e0c4e9923965cfcfeb26a60d18555acf6
Anas Ismail Khan <anasismail28@gmail.com>
2024-11-27 21:49:08 +0500

    Added Macbook, disabled the stupid services grid. Any other new elements are merely side-effects of that. In other news, finally gave in and installed ScrollMagic, Moved the now mostly unneeded inline script to script.js, Moved logos from /logos to /clients. Disabled the stupid glass overlay, We also have a couple of new fonts just sitting there unused for now.

---
+++ js/macbook.js
@@ -0,0 +1,197 @@
+function at(progress, trigger) {
+    return progress > trigger - epsilon && progress < trigger + epsilon;
+}
+const macbook = document.querySelector(".macbook");
+const contents = Object.values(document.querySelectorAll(".contentInner"));
+const clients = document.querySelector("#clients");
+const hero = document.querySelector("#hero");
+const logos = document.querySelector(".logos");
+
+const epsilon = 0.01;
+const duration = 4000;
+const fractions = [0.1, 0.4, 0.7];
+const offsets = fractions.map((fraction) => duration * fraction);
+const durations = [0.3 * duration, 0.3 * duration, 0.3 * duration];
+const controller = new ScrollMagic.Controller();
+
+const deviceScenes = [
+    {
+        fraction: 0.1,
+        FORWARD: () => {
+            macbook.classList.remove("closed");
+        },
+        BACKWARD: () => {
+            macbook.classList.add("closed");
+        },
+    },
+    {
+        fraction: 0.4,
+        FORWARD: () => {
+            macbook.classList.add("imac");
+        },
+        BACKWARD: () => {
+            macbook.classList.remove("imac");
+        },
+    },
+    {
+        fraction: 0.7,
+        FORWARD: () => {
+            macbook.classList.add("iphone");
+        },
+        BACKWARD: () => {
+            macbook.classList.remove("iphone");
+        },
+    },
+];
+
+//#region device
+new ScrollMagic.Scene({ triggerElement: "#protagonist", duration })
+    .setPin("#apple-magic")
+    .on("progress", function (event) {
+        deviceScenes.forEach((scene) => {
+            if (event.progress > scene.fraction) {
+                scene["FORWARD"]();
+            } else {
+                scene["BACKWARD"]();
+            }
+        });
+    })
+    .addIndicators() // add indicators (requires plugin)
+    .addTo(controller);
+//#endregion
+
+//#region screen content
+new ScrollMagic.Scene({
+    triggerElement: "#protagonist",
+    offset: offsets[0],
+    duration: durations[0],
+})
+    .setClassToggle(".content-inner.first", "active")
+    .addIndicators() // add indicators (requires plugin)
+    .addTo(controller);
+
+new ScrollMagic.Scene({
+    triggerElement: "#protagonist",
+    offset: offsets[1],
+    duration: durations[0],
+})
+    .setClassToggle(".content-inner.second", "active")
+    .addIndicators() // add indicators (requires plugin)
+    .addTo(controller);
+
+new ScrollMagic.Scene({
+    triggerElement: "#protagonist",
+    offset: offsets[2],
+    duration: 1600,
+})
+    .setClassToggle(".content-inner.third", "active")
+    .addIndicators() // add indicators (requires plugin)
+    .addTo(controller);
+//#endregion
+
+//#region heading content
+new ScrollMagic.Scene({
+    triggerElement: "#protagonist",
+    offset: offsets[0],
+    duration: durations[0],
+})
+    .setClassToggle(".heading-inner.first", "active")
+    .addIndicators() // add indicators (requires plugin)
+    .addTo(controller);
+
+new ScrollMagic.Scene({
+    triggerElement: "#protagonist",
+    offset: offsets[1],
+    duration: durations[0],
+})
+    .setClassToggle(".heading-inner.second", "active")
+    .addIndicators() // add indicators (requires plugin)
+    .addTo(controller);
+
+new ScrollMagic.Scene({
+    triggerElement: "#protagonist",
+    offset: offsets[2],
+    duration: 1600,
+})
+    .setClassToggle(".heading-inner.third", "active")
+    .addIndicators() // add indicators (requires plugin)
+    .addTo(controller);
+//#endregion
+
+//#region footer content
+new ScrollMagic.Scene({
+    triggerElement: "#protagonist",
+    offset: offsets[0],
+    duration: durations[0],
+})
+    .setClassToggle(".foot-inner.first", "active")
+    .addIndicators() // add indicators (requires plugin)
+    .addTo(controller);
+
+new ScrollMagic.Scene({
+    triggerElement: "#protagonist",
+    offset: offsets[1],
+    duration: durations[0],
+})
+    .setClassToggle("#font-well", "active")
+    .addIndicators() // add indicators (requires plugin)
+    .addTo(controller);
+//#endregion
+
+//#region colors
+new ScrollMagic.Scene({
+    triggerElement: "#protagonist",
+    offset: offsets[1],
+    duration: durations[0],
+})
+    .setClassToggle("#protagonist", "purple-bg")
+    .addIndicators() // add indicators (requires plugin)
+    .addTo(controller);
+
+new ScrollMagic.Scene({
+    triggerElement: "#protagonist",
+    offset: offsets[2],
+    duration: durations[0],
+})
+    .setClassToggle("#protagonist", "green-bg")
+    .addIndicators() // add indicators (requires plugin)
+    .addTo(controller);
+
+new ScrollMagic.Scene({
+    triggerElement: "#clients",
+    triggerHook: 1,
+    duration: clients.offsetHeight,
+})
+    .setClassToggle("#protagonist", "black-bg")
+    .addIndicators() // add indicators (requires plugin)
+    .addTo(controller);
+//#endregion
+
+new ScrollMagic.Scene({
+    triggerElement: "#clients",
+    duration: clients.offsetHeight,
+})
+    .on("progress", function (event) {
+        logos.style.transform =
+            "translateY(" + event.progress * -clients.offsetHeight + "px)";
+    })
+    .addIndicators() // add indicators (requires plugin)
+    .addTo(controller);
+
+new ScrollMagic.Scene({
+    triggerHook: 0,
+    triggerElement: "#hero",
+    duration: hero.offsetHeight,
+})
+    .on("progress", function (event) {
+        if (svgPaths.length)
+            svgPaths.map((d, i) => {
+                const direction = i % 2 === 0 ? 1 : -1;
+                const translateY =
+                    direction * event.progress * clients.offsetHeight;
+                const factor = 10;
+                d.style.transform = `translateY(${translateY / factor}px)`;
+            });
+    })
+    .addIndicators() // add indicators (requires plugin)
+    .addTo(controller);
--- js/script.js
+++ js/script.js
@@ -6,3 +6,71 @@ var typed = new Typed("#rotating-head", {
     startDelay: 500,
     loop: true,
 });
+
+const logoContainer = document.querySelector(".logos");
+const mySvg = document.getElementById("hero-bg-obj");
+
+function svgMagic() {
+    Object.values(
+        mySvg.contentDocument?.querySelectorAll("#layer1 > g") ?? {}
+    ).map((path) => {
+        svgPaths.push(path);
+    });
+}
+
+function mouseMagic(e) {
+    const mouseX = e.clientX;
+    const midX = window.innerWidth / 2;
+    const differential = mouseX - midX;
+
+    if (svgPaths.length)
+        svgPaths.map((d, i) => {
+            const direction = i % 2 === 0 ? 1 : -1;
+            d.style.transform = `translateX(${
+                (direction * differential) / 50
+            }px)`;
+        });
+}
+
+function scrollMagic() {
+    const coin = Math.round(Math.random());
+    const scrollPosition = window.scrollY;
+    const mousePosition = window.mouseX;
+    const difference = scrollPosition - window.innerHeight * 0.7;
+    const difference2 = scrollPosition - window.innerHeight * 1.6;
+    const translation = difference2 * 3;
+
+    if (scrollPosition > 0.5 * window.innerHeight)
+        Object.values(document.querySelectorAll("#services>div")).map((d) =>
+            d.classList.add("pulsar")
+        );
+    if (difference > 1)
+        logoContainer.style.transform = `translateY(-${difference / 2}px)`;
+    if (difference2 > 1)
+        if (translation < 700)
+            document.querySelector(
+                ".testim"
+            ).style.transform = `translateY(${translation}px)`;
+        else
+            document.querySelector(
+                ".testim"
+            ).style.transform = `translateY(700px)`;
+    if (svgPaths.length)
+        svgPaths.map((d, i) => {
+            const direction = i % 2 === 0 ? 1 : -1;
+            d.style.transform = `translateY(${
+                (direction * scrollPosition) / 20
+            }px)`;
+        });
+}
+
+let svgPaths = [];
+
+mySvg.addEventListener("load", svgMagic);
+// window.addEventListener('scroll', scrollMagic);
+// document.getElementById("hero").addEventListener('mousemove', mouseMagic);
+document.addEventListener("DOMContentLoaded", function (event) {
+    svgMagic();
+    // scrollMagic();
+});
+svgMagic();
